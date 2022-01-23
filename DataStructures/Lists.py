6e236f2b355730c44fd2d23d9b612c5d901426bd
markb <markbanford@gmail.com>
2022-01-23 21:27:49 +0000

    Initial commit

---
+++ DataStructures/Lists.py
@@ -0,0 +1,16 @@
+'''List  [1,2,3]
+- General Purpose
+- Most widely used data structure
+- Grow and shrink as needed
+- Sequence type
+- Sortable'''
+
+# [start:end+1:step]
+
+x = 'computer'
+
+print(x[1:4])  # returns 1st to 3rd, based on index
+print(x[1:6:2])  # returns 1st to 5th, gives every other item
+print(x[3:])  # returns from 3rd to no end
+print(x[:5])  # returns items 0 to 4
+print(x[-1])  # returns last item
---
+++ DataStructures/Slicing.py
@@ -0,0 +1,12 @@
+# [start:end+1:step]
+
+x = 'computer'
+
+print(x[1:4])  # returns 1st to 3rd, based on index
+print(x[1:6:2])  # returns 1st to 5th, gives every other item
+print(x[3:])  # returns from 3rd to no end
+print(x[:5])  # returns items 0 to 4
+print(x[-1])  # returns last item
+print(x[::-1])  # reverse string
+print(x[-3:])  # last 3 items
+print(x[:-2])  # all except last 2 items

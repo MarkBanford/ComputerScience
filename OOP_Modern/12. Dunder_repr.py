53ee9c6dcc87efe25585676a207cd9d080b316f5
markb <markbanford@gmail.com>
2022-04-10 19:31:04 +0100

    1

---
+++ OOP_Modern/12. Dunder_repr.py
@@ -0,0 +1,18 @@
+class Book:
+    def __init__(self, title, author, book_type, pages):
+        self.title = title
+        self.author = author
+        self.book_type = book_type
+        self.pages = pages
+
+    def __repr__(self):
+        """Without this the output would be
+        <__main__.Book object at 0x000001B861E56F70>
+        The structure should resemble the class creation syntax
+        Book('Cars for beginners','Nassim','Hardcover',519)"""
+        return f"Book('{self.title}','{self.author}','{self.book_type}',{self.pages})"
+
+
+b = Book("Cars for beginners", "Nassim", "Hardcover", 519)
+
+print(eval(repr(b)))

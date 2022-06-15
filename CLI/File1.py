a993add5cf293f825d69752e907eab50c443e975
markb <markbanford@gmail.com>
2022-06-15 22:10:47 +0100

    1

--- CLI/File1.py
+++ CLI/File1.py
@@ -1,8 +1,6 @@
 import sys
 from typing import List, Dict
 
-print(sys.argv)  # current file
-
 
 # Run program as a module with two command-line arguments
 # 1. Name of the file to search
@@ -13,7 +11,8 @@ print(sys.argv)  # current file
 def main() -> None:
     """Entry point of program run as module"""
     args: Dict[str, str] = read_args()
-    print(args)
+    results: List[str] = search_file(args["file_path"], args["keyword"])
+    print(len(results))
 
 
 def read_args() -> Dict[str, str]:
@@ -25,6 +24,19 @@ def read_args() -> Dict[str, str]:
         "file_path": sys.argv[1],
         "keyword": sys.argv[2]
     }
+
+
+def search_file(file_path: str, keyword: str) -> List[str]:
+    """Open file_path reads each line and returns a list of lines"""
+    matches: List[str] = []
+    file_handle = open(file_path, "r", encoding="utf8")
+    for line in file_handle:
+        if keyword in line:
+            matches.append(line)
+
+    file_handle.close()
+
+    return matches
 
 
 if __name__ == '__main__':
